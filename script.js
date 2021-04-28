794fa688effecdbdb37f8692d32824ae64156b19
Jerry Plotke <jerry.plotke@gmail.com>
2021-04-28 11:06:25 -0500

    added pseudo code

---
+++ script.js
@@ -0,0 +1,7 @@
+// create a button that will start a timer once clicked and presents a question
+// answer multiple choice question by clicking answer.
+// once answer is chosen screen will say "correct" or "wrong" then list another question
+// if answer is incorrect then time will be subtracted from clock
+// quiz ends when timer reaches 0 or when questeds are all answered
+// final score will be posted
+// enter initials into field to save high scores
