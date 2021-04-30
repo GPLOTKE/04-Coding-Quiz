47d23d274bb6b87d7b434fde56b5f5b95865ae86
Jerry Plotke <jerry.plotke@gmail.com>
2021-04-30 11:09:04 -0500

    timer initiated upon click

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
 // create a button that will start a timer once clicked and presents a question
+
+
+document.getElementById("startQuiz").addEventListener("click", function(){
+    var secondsleft = 75;
+
+    var downloadTimer = setInterval(function function1(){
+    document.getElementById("startTimer").innerHTML = secondsleft + " seconds remaining.";
+
+    secondsleft -= 1;
+    if(secondsleft <= 0){
+        clearInterval(downloadTimer);
+        document.getElementById("startTimer").innerHTML = "Time is up!"
+    }
+    }, 1000);
+
+    console.log(startTimer);
+});
+
+
+// function promptEl(){
+//     var questionEl = prompt("Type A, B, C or D. Commnly used data types DO NOT include:");
+//     alert (questionEl);
+// }
 // answer multiple choice question by clicking answer.
 // once answer is chosen screen will say "correct" or "wrong" then list another question
 // if answer is incorrect then time will be subtracted from clock
 // quiz ends when timer reaches 0 or when questeds are all answered
 // final score will be posted
-// enter initials into field to save high scores
+// enter initials into field to save high score
