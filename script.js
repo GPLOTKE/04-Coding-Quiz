27f51e216bb122b71302e2ba0e8c241b375df8e9
Jerry Plotke <jerry.plotke@gmail.com>
2021-04-30 15:35:10 -0500

    added button click variables and functions

--- script.js
+++ script.js
@@ -1,12 +1,52 @@
 // create a button that will start a timer once clicked and presents a question
-var hideContainerEl = $('#instructions');
-var showQuestionEl = $('#question1');
+var showInstructionsEl = $('#instructions');
+var showQuestion1El = $('#question1');
+var showQuestion2El = $('#question2');
+var showQuestion3El = $('#question3');
+var showQuestion4El = $('#question4');
+var showQuestion5El = $('#question5');
+var showAnswerBtns1El = $('#answerBtns1');
+var showAnswerBtns2El = $('#answerBtns2');
+var showAnswerBtns3El = $('#answerBtns3');
+var showAnswerBtns4El = $('#answerBtns4');
+var showAnswerBtns5El = $('#answerBtns5');
+var showFinalScoresEl = $('#finalScores');
 
-showQuestionEl.hide();
+showQuestion1El.hide();
+showQuestion2El.hide();
+showQuestion3El.hide();
+showQuestion4El.hide();
+showQuestion5El.hide();
+showFinalScoresEl.hide();
 
-hideContainerEl.on('click', function () {
-    hideContainerEl.hide();
-    showQuestionEl.show();
+showInstructionsEl.on('click', function () {
+    showInstructionsEl.hide();
+    showQuestion1El.show();
+});
+
+showAnswerBtns1El.on('click', function() {
+    showQuestion1El.hide();
+    showQuestion2El.show();
+});
+
+showAnswerBtns2El.on('click', function() {
+    showQuestion2El.hide();
+    showQuestion3El.show();
+});
+
+showAnswerBtns3El.on('click', function() {
+    showQuestion3El.hide();
+    showQuestion4El.show();
+});
+
+showAnswerBtns4El.on('click', function() {
+    showQuestion4El.hide();
+    showQuestion5El.show();
+});
+
+showAnswerBtns5El.on('click', function() {
+    showQuestion5El.hide();
+    showFinalScoresEl.show();
 });
 
 document.getElementById("startQuiz").addEventListener("click", function () {
@@ -26,10 +66,6 @@ document.getElementById("startQuiz").addEventListener("click", function () {
 });
 
 
-// function promptEl(){
-//     var questionEl = prompt("Type A, B, C or D. Commnly used data types DO NOT include:");
-//     alert (questionEl);
-// }
 // answer multiple choice question by clicking answer.
 // once answer is chosen screen will say "correct" or "wrong" then list another question
 // if answer is incorrect then time will be subtracted from clock
